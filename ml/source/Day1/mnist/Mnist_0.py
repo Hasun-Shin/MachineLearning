981f3c933fd71db507e1e07d6371c9f00adeb399
Your Name <hahahasony@naver.com>
2020-04-08 17:54:44 +0900

    200408 mnist basic materials

---
+++ ml/source/Day1/mnist/Mnist_0.py
@@ -0,0 +1,16 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt # pip install matplotlib
+
+# print(os.getcwd())
+data_file = open("../dataset/mnist_train_100.csv", "r")
+data_list = data_file.readlines()
+data_file.close()
+
+all_values = data_list[1].split(',')
+# float 데이터 형태를 원소로 가지는 배열을 만들고 싶을 때는 np.asfarray() 함수를 사용
+image_array = np.asfarray(all_values[1:]).reshape((28,28))
+plt.imshow(image_array, cmap='Greys', interpolation='None')
+plt.show()
+scaled_input = (np.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01
+print(scaled_input)
